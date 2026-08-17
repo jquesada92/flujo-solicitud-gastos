d33e1f752b10ae0b01238386cc8132bab996f508
Jose Quesada <jaquesada92@outlook.com>
2026-08-17 17:57:36 -0500

    test: require request type repair migration in Alembic chain

--- backend/tests/test_migrations.py
+++ backend/tests/test_migrations.py
@@ -12,8 +12,9 @@ class MigrationTopologyTests(unittest.TestCase):
         config.set_main_option('script_location', str(backend_dir / 'alembic'))
         script = ScriptDirectory.from_config(config)
 
-        self.assertEqual(script.get_heads(), ['20260817_0002'])
+        self.assertEqual(script.get_heads(), ['20260817_0003'])
         revisions = {revision.revision: revision.down_revision for revision in script.walk_revisions()}
+        self.assertEqual(revisions['20260817_0003'], '20260817_0002')
         self.assertEqual(revisions['20260817_0002'], '20260817_0001')
         self.assertEqual(revisions['20260817_0001'], '20260817_0000')
         self.assertIsNone(revisions['20260817_0000'])
